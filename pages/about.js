f481619afd42bad8875d851936f43e8eb1215910
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-01-28 22:43:41 +0900

    _app.js와 전역에 CSS 설정

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-import NavBar from "../components/NavBar";
-
 export default function Potato() {
   return (
     <div>
-      <NavBar></NavBar>
+      <h1>About</h1>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import NavBar from "../components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar></NavBar>
+      <h1>Index</h1>
     </div>
   );
 }
